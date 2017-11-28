7fd12d8b1b1289161e3bbed8d3c2fb0719ebcdaa
Michael Berk <michael.berk98@web.de>
2017-11-28 09:15:47 +0100

    AdminMaske of Penetration

--- Autosausleihen/AdminMaske.Designer.cs
+++ Autosausleihen/AdminMaske.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // AdminMaske
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 339);
+            this.Name = "AdminMaske";
             this.Text = "AdminMaske";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
